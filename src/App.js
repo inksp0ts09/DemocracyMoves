541a5744bfe12e4caca0ffc4fc899aa7c9e2165c
Maryanne McGlone <92317108+inksp0ts09@users.noreply.github.com>
2023-05-30 22:08:35 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import "./Home.js";
+import Home from "./Home.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Democracy Moves</h1>
+      <Home />
+    </div>
+  );
+}
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <h2>hello world</h2>
+    </div>
+  );
+}
